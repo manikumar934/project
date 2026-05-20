6dc8e7f2847e4814b2f82abd2cd984cfbe3edc92
manikumar <manikumar934@gmail.com>
2026-05-20 16:07:36 +0530

    6

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -27,4 +27,8 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for r in aws_route53_record.cert_validation : r.fqdn]
+
+  timeouts {
+    create = "2h"
+  }
 }
